20475186f8f6ef6f41fc0a17c2be7d129475169e
Ajay-016 <146326621+Ajay-016@users.noreply.github.com>
2025-04-17 16:50:51 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict" // treat all JS code as newer version.
+
+let name = "Ajay"
+let age = 22
+let isLoggedIn = false
+let state = null // empty
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value is not assigned
+// symbol => use for uniqueness
+
+
+// object
+console.log(typeof "Ajay");
+console.log(typeof age);
+console.log(typeof null); // output is object
+console.log(typeof undefined); // output is undefined
+
+
+
+
+
+
+
