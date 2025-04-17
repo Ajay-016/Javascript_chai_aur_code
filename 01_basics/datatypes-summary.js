39fe0d872a99c070692a8e43f2ab9043743f53a2
Ajay-016 <146326621+Ajay-016@users.noreply.github.com>
2025-04-17 20:21:00 +0000

    datatypes  summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,48 @@
+// Datatypes is divided in 2 ways of how they are being stored in memory and accessed 
+// (1) Primitive ---> Call by value  
+// (2) Non-Primitive / Reference Type
+
+// Primitive
+// 7 Types : String , Number , Boolean , null , undefined , Symbol , BigInt 
+
+
+const score = 100
+const scoreValue = 100.3
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id==anotherId);
+
+
+const bigNumber = 348756287658723465786n // use n in last of number to convert it into bigint
+
+
+
+// IMPORTANT : JavaScript is a dynamically typed language.
+//That means you don’t have to declare variable types explicitly — the type is determined at runtime based on the value assigned.
+//TypeScript — it’s basically JavaScript with static typing added on top and it’s enforced at compile-time, not runtime.
+
+
+
+// Reference Type (Non Primitive) ----> call by reference
+// Array , Objects , Functions 
+// Note : Learn about Objects and Web Events in JS to master it .
+
+const heroes = ["shaktiman" , "naagraj" , "doga"] // Array
+let myObj = { // Object
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){ //functions
+    console.log("Hello world");
+}
+
+console.log(typeof bigNumber);
+console.log(typeof outsideTemp);
+console.log(typeof myFunction);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
