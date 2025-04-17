d9bfd982bc7acd1a5d60368aa09719ec253ac87b
Ajay-016 <146326621+Ajay-016@users.noreply.github.com>
2025-04-17 17:17:29 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,35 @@
+// Always do investigation study in JavaScript.
+
+
+let score = "33abc"
+let random = null
+console.log(typeof score);
+console.log(typeof(score)); // both are same.
+
+let valueInNumber = Number(score) // conversion to number 
+let random_to_number = Number(random)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+console.log(random_to_number);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+// null => 0;
+// undefined => NaN
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber);
+
+
