fb8af7549a9dd1b9aaec00f97536d83fcbfed7cc
Ajay-016 <146326621+Ajay-016@users.noreply.github.com>
2025-04-17 18:03:08 +0000

    Operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -3,14 +3,14 @@
 
 let score = "33abc"
 let random = null
-console.log(typeof score);
-console.log(typeof(score)); // both are same.
+// console.log(typeof score);
+// console.log(typeof(score)); // both are same.
 
 let valueInNumber = Number(score) // conversion to number 
 let random_to_number = Number(random)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
-console.log(random_to_number);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+// console.log(random_to_number);
 
 // "33" => 33
 // "33abc" => NaN
@@ -20,7 +20,7 @@ console.log(random_to_number);
 
 let isLoggedIn = 1
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
@@ -29,7 +29,43 @@ console.log(booleanIsLoggedIn);
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber);
+// console.log(stringNumber)
+// console.log(typeof stringNumber);
+
+// ***********************Operations****************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+// shift + alt + down key , to copy current line in the next line
+// console.log(2+2);   
+// console.log(2-2);   
+// console.log(2*2);   
+// console.log(2/2);  
+// console.log(2**2);   // 2^2
+// console.log(2**3);   // 2^3
+// console.log(2%3); // remainder
 
 
+let str1 = "hello"
+let str2 = " ajay"
+let str3 = str1 + str2
+// console.log(str3);
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log(1 + 2 + "2");
+// console.log(3 + 4 * 5 % 3); // good for questions but in real not even a single person will appreciate this , why to complicate things when we use parenthesis for it.
+// console.log(+true);
+// console.log(-true);
+// console.log(+"");
+
+let num1 , num2 , num3
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
+gameCounter--
+console.log(gameCounter);
